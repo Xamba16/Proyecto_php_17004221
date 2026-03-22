7f2157cae37c76ff2a5e483e8d691859b6319775
Xamba16 <17004221@galileo.edu>
2026-03-22 23:34:08 +0000

    Se agrega código e imagen

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  // Mostramos el saludo en pantalla
+  echo "<h1>Hola mundo, soy Melvin Susana Monroy Xambá</h1>";
+
+  // Insertamos la imagen que está en la misma carpeta
+  echo '<img src="Gemini_Generated_Image_mxa5m2mxa5m2mxa5.png" alt="Imagen de mi proyecto" style="max-width: 500px; height: auto;">';
+?>
